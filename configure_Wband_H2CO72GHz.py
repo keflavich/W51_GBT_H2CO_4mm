4a154a834d69e49aa64bd5e769777a81e5c6a34e
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-05-08 14:19:18 +0200

    rmeove commented out blocks

--- configure_Wband_H2CO72GHz.py
+++ configure_Wband_H2CO72GHz.py
@@ -2,55 +2,6 @@
 # 4mm line setup FL1 72GHz
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-# Original:
-"""
-receiver  = 'Rcvr68_92'
-beam      = 'B12'
-obstype   = 'Spectroscopy'
-backend   = 'Spectrometer'
-nwin      = 1
-restfreq  = 72000.
-deltafreq = 0
-bandwidth = 800
-swmode    = "tp_nocal"
-swtype    = "none"
-swper     = 0.2
-swfreq    = 0, 0
-tint      = 2.0
-vlow      = 0.
-vhigh     = 0.
-vframe    = "lsrk"
-vdef      = "Radio"
-pol       = "Linear"
-nchan  =  "high"
-spect.levels = 3
-"""
-# KFPA + VEGAS
-"""
-receiver = 'RcvrArray18_26'
-beam = 'all'
-obstype = 'Spectroscopy'
-backend = 'VEGAS'
-dopplertrackfreq = 23694.47
-swmode = "tp"
-swtype = "none"
-swper = 0.5
-swfreq =0.0,0.0
-tint = 1.0
-broadband = 0
-vlow = 0
-vhigh = 0
-vframe = "lsrk"
-vdef = "Radio"
-noisecal = "lo"
-pol = "Circular"
-vegas.vpol='self'
-vegas.subband = 8
-nchan = "low"
-bandwidth = 23.44
-deltafreq = 0
-restfreq = 23694.47, 23722.63, 23870.13, 24139.42, 24532.99, 24506.28, 24515.00, 23780.92
-"""
 
 # H2CO 1-0 72.83795
 # H2CO 514-515 72.4091
